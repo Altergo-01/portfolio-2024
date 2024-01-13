b475b79533b667b207ac380cc61fdcf777bf2a1c
Altergo-01 <axelbreheret@gmail.com>
2024-01-13 17:30:38 +0100

    navbar and base structure

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
-<script setup>
+<script  >
 import { RouterLink, RouterView } from 'vue-router'
- 
+import NavBar from './components/NavBar.vue';
+import ContentSwitch from './components/ContentSwitch.vue';
+export default {
+    components: {
+      NavBar,
+      ContentSwitch,
+    },
+    data() {
+        return {
+        }
+    },
+    methods: {
+            
+    },
+
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-        
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <NavBar></NavBar>
+  <ContentSwitch></ContentSwitch>
+   
 </template>
 
 <style scoped>
- 
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import gsap from 'gsap';
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+app.config.globalProperties.$gsap = gsap;
 app.use(createPinia())
 app.use(router)
 
